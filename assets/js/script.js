436fb7f26fa91fe98b8c264a9ba8b8e9acb28155
root <root@MSI.localdomain>
2018-09-12 13:37:34 -0400

    added support for office air quality monitoring, and a messageboard that can be written to from the website that is displayed in the office

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@ var count = 0;
 var ledStatus = 0;
 
 var databaseRef = firebase.database().ref();
+var temp = document.getElementById("temp");
+var humidity = document.getElementById("humidity");
+var heatIndex = document.getElementById("head-index");
+var writeLCD = document.getElementById("writeLCD");
 
 document.getElementById("toggleLightButton").addEventListener("click", function(){
   if(count % 2 == 0){
@@ -9,5 +13,19 @@ document.getElementById("toggleLightButton").addEventListener("click", function(
   }else ledStatus = 0;
   count++;
   databaseRef.child("status").set(ledStatus);
-  console.log(ledStatus);
 });
+
+
+document.getElementById("textWriteButton").addEventListener("click", function(){
+  databaseRef.child("message").set(writeLCD.value);
+  writeLCD.value = "";
+});
+
+
+window.onload = function(){
+  databaseRef.on("value", function(snapshot){
+    temp.innerHTML = snapshot.child("temp").node_.value_;
+    humidity.innerHTML = snapshot.child("humidity").node_.value_;
+    heatIndex.innerHTML = snapshot.child("hif").node_.value_;
+  });
+};
