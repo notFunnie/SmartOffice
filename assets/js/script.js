139026db096580453a1b0e8abf5ef28e984bca49
notFunnie <33010049+notFunnie@users.noreply.github.com>
2018-09-14 13:36:31 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,22 @@ var humidity = document.getElementById("humidity");
 var heatIndex = document.getElementById("head-index");
 var writeLCD = document.getElementById("writeLCD");
 
-document.getElementById("toggleLightButton").addEventListener("click", function(){
-  if(count % 2 == 0){
-    ledStatus = 1;
-  }else ledStatus = 0;
-  count++;
-  databaseRef.child("status").set(ledStatus);
+databaseRef.on("value", function(snapshot){
+    ledStatus = snapshot.child("status").node_.value_;    
 });
 
+document.getElementById("toggleLightButton").addEventListener("click", function(){
+ 	if (ledStatus == 0){
+        ledStatus = 1;
+    }
+    else if(ledStatus == 1){
+        ledStatus = 0;
+    }
+    else{ //In case the ledStatus has a state other than 1 or 0
+        ledStatus = 0;
+    }       
+    databaseRef.child("status").set(ledStatus);
+});
 
 document.getElementById("textWriteButton").addEventListener("click", function(){
   databaseRef.child("message").set(writeLCD.value);
